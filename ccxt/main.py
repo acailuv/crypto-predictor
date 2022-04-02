9f452289a6d2778ed4c4ea597b172e87a0f11f39
Dave Hong <liangcai.stdnt@gmail.com>
2022-04-02 20:54:31 +0700

    Added CCXT Prices Data GET Prototype

---
+++ ccxt/main.py
@@ -0,0 +1,36 @@
+import ccxt
+import time
+import pandas as pd
+import numpy as np
+
+indodax = ccxt.indodax()
+
+PAIR_SYMBOL = 'BTC/IDR'
+TIMEFRAME = '1m'
+TIMEFRAME_MULTI = 60*1000
+START_TIME = indodax.parse8601('2022-01-10 00:00:00')
+END_TIME = indodax.parse8601('2022-01-10 03:00:00')
+
+DELAY = 15
+
+data = []
+
+candle_no = (int(END_TIME) - int(START_TIME)) / TIMEFRAME_MULTI + 1
+print('Downloading...')
+while START_TIME < END_TIME:
+    try:
+        ohlcvs = indodax.fetch_ohlcv(PAIR_SYMBOL, TIMEFRAME, START_TIME)
+        START_TIME += len(ohlcvs) * TIMEFRAME_MULTI
+        data += ohlcvs
+        print(str(len(data)) + ' of ' + str(int(candle_no)) + ' candles loaded...')
+    except (ccxt.ExchangeError, ccxt.AuthenticationError, ccxt.ExchangeNotAvailable, ccxt.RequestTimeout) as error:
+        print('Got an error', type(error).__name__, error.args, ', retrying in', DELAY, 'seconds...')
+        time.sleep(DELAY)
+
+header = ['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume']
+df = pd.DataFrame(data, columns=header)
+
+print(df)
+
+open('BTCIDR.txt', 'w')
+np.savetxt('BTCIDR.txt', df.Open, fmt='%.8f')
