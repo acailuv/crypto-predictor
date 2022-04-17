3a17500ce3d984779e20477e9043db5826555f70
Dave Hong <liangcai.stdnt@gmail.com>
2022-04-17 22:45:49 +0700

    Modularize CCXTIndodaxEngine

--- ccxt/ccxt_engine.py
+++ ccxt/ccxt_engine.py
@@ -4,12 +4,16 @@ import pandas as pd
 import ccxt
 import utils
 
-TIMEFRAME = '1m'
+TIMEFRAME = "1m"
 TIMEFRAME_MULTI = 60*1000
 RESOURCE_FOLDER = "./res"
 ERROR_DELAY = 15
 
-class CCXTEngine:
+DOWNTREND = 0
+SIDEWAYS = 1
+UPTREND = 2
+
+class CCXTIndodaxEngine:
   def __init__(self, start_year, end_year, pair_symbol, debug=False):
     self.indodax = ccxt.indodax()
     self.pair_symbol = pair_symbol
@@ -38,6 +42,56 @@ class CCXTEngine:
     except:
       return False
 
+  def download_data(self):
+    data = []
+
+    candle_no = (int(self.end_time) - int(self.start_time)) / TIMEFRAME_MULTI + 1
+
+    if self.debug:
+      print("Downloading...")
+    
+    while self.start_time < self.end_time:
+      try:
+        candles = self.indodax.fetch_ohlcv(self.pair_symbol, TIMEFRAME, self.start_time)
+        self.start_time += len(candles) * TIMEFRAME_MULTI
+        data += candles
+
+        if self.debug:
+          print(str(len(data)) + " of " + str(int(candle_no)) + " candles loaded...")
+      except (ccxt.ExchangeError, ccxt.AuthenticationError, ccxt.ExchangeNotAvailable, ccxt.RequestTimeout) as error:
+        print("Got an error", type(error).__name__, error.args, ", retrying in", ERROR_DELAY, "seconds...")
+        time.sleep(ERROR_DELAY)
+    
+    return data
+  
+  def evaluate_trends(self, data):
+    data_with_trend = []
+
+    for candle in data:
+      current_candle = candle
+      open_price = candle[1]
+      close_price = candle[4]
+
+      if open_price < close_price:
+        current_candle.append(UPTREND)
+      elif open_price == close_price:
+        current_candle.append(SIDEWAYS)
+      else:
+        current_candle.append(DOWNTREND)
+      
+      data_with_trend.append(current_candle)
+    
+    if self.debug:
+      print(f"\n\nTrend Processing Complete!\n> Results:\n{data_with_trend}")
+  
+    return data_with_trend
+
+  def transform_to_dataframe(self, data_with_trend):
+    header = ["Timestamp", "Open", "High", "Low", "Close", "Volume", "Trend"]
+    data_df = pd.DataFrame(data_with_trend, columns=header)
+
+    return data_df
+
   def load_data(self):
     if self.check_file_in_directory():
       res = pd.read_pickle(f"{RESOURCE_FOLDER}/{self.file_name}")
@@ -49,27 +103,9 @@ class CCXTEngine:
     if self.debug:
       print("\n\nData File Not Found! Downloading from CCXT...")
 
-    data = []
-
-    candle_no = (int(self.end_time) - int(self.start_time)) / TIMEFRAME_MULTI + 1
-
-    if self.debug:
-      print('Downloading...')
-    
-    while self.start_time < self.end_time:
-        try:
-            ohlcvs = self.indodax.fetch_ohlcv(self.pair_symbol, TIMEFRAME, self.start_time)
-            self.start_time += len(ohlcvs) * TIMEFRAME_MULTI
-            data += ohlcvs
-
-            if self.debug:
-              print(str(len(data)) + ' of ' + str(int(candle_no)) + ' candles loaded...')
-        except (ccxt.ExchangeError, ccxt.AuthenticationError, ccxt.ExchangeNotAvailable, ccxt.RequestTimeout) as error:
-            print('Got an error', type(error).__name__, error.args, ', retrying in', ERROR_DELAY, 'seconds...')
-            time.sleep(ERROR_DELAY)
-
-    header = ['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume']
-    data_df = pd.DataFrame(data, columns=header)
+    data = self.download_data()
+    data_with_trend = self.evaluate_trends(data)
+    data_df = self.transform_to_dataframe(data_with_trend)
 
     data_df.to_pickle(f"{RESOURCE_FOLDER}/{self.file_name}")
 
--- ccxt/main.py
+++ ccxt/main.py
@@ -1,4 +1,4 @@
 import ccxt_engine as ce
 
-ccxt_engine = ce.CCXTEngine(2022, 2022, 'BTC/IDR', True)
+ccxt_engine = ce.CCXTIndodaxEngine(2022, 2022, 'BTC/IDR', True)
 ccxt_engine.load_data()
